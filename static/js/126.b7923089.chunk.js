3b06072f3812309a73d6a3a5fdf594bfbb4c8d67
stargazinghero <87721107+stargazinghero@users.noreply.github.com>
2022-06-06 04:44:27 +0000

    Deploying to gh-pages from @ stargazinghero/goit-react-hw-08-phonebook@821900b71fcef03d393eaa02e73143f5405e141c 🚀

---
+++ static/js/126.b7923089.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[126],{4126:function(e,t,n){n.r(t),n.d(t,{default:function(){return a}});var r=n(3504),o=n(3329);function a(){return(0,o.jsxs)(o.Fragment,{children:[(0,o.jsx)("h1",{children:"Page not found. Go to Home page"}),(0,o.jsx)(r.rU,{to:"/",children:"Home"})]})}}}]);
+//# sourceMappingURL=126.b7923089.chunk.js.map
